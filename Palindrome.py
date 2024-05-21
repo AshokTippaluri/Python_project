72b2c9e6f9cd923d3001ccf33abe87e38d75be5f
Ashok  Chandra Reddy Tippaluri <96752472+AshokTippaluri@users.noreply.github.com>
2024-05-21 14:11:47 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,16 +1,8 @@
-# def is_palindrome(s):
-#     s = ''.join(c.lower() for c in s if c.isalnum())  # Remove non-alphanumeric characters and convert to lowercase
-#     return s == s[::-1]
-
-# # Test the function
-# print(is_palindrome("A man, a plan, a canal: Panama"))  # Output: True
-# print(input(is_palindrome("Enter the input : ")))  # Output: True
-
-#Taking the input fro the user and converting into the lower case
+#Taking the input from the user and converting it into the lowercase
 s = input ("Please enter the input : ").lower() 
 #Slicing the given string and assign to the reverse
 reverse = s[::-1]
-#checking the given string is equal to reverse
+#checking the given string is equal to the reverse
 if (s == reverse):
     print("It is a palindrome")
 else:
