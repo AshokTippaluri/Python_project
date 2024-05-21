095cbf73771880dd89010d7cff862b2935df58fd
Ashok  Chandra Reddy Tippaluri <96752472+AshokTippaluri@users.noreply.github.com>
2024-05-21 14:12:38 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -16,22 +16,22 @@ def get_ram_usage():
     # Percentage of used memory
     used_memory_percentage = memory.percent
 
-    # Convert memory sizes from bytes to human-readable format (MB)
-    total_memory_mb = total_memory / (1024 * 1024)
-    available_memory_mb = available_memory / (1024 * 1024)
-    used_memory_mb = used_memory / (1024 * 1024)
+    # Convert memory sizes from bytes to human-readable format (GB)
+    GB_conveter = 1024 ** 3 # bytes to kb, kb to mb, mb to GB
+    total_memory_gb = total_memory / GB_conveter
+    available_memory_gb = available_memory / GB_conveter
+    used_memory_gb = used_memory / GB_conveter
 
     # Convert memory in percentages %
-    available_memory_percent = ((total_memory_mb-used_memory_mb)/total_memory_mb)*100
+    available_memory_percent = ((total_memory_gb-used_memory_gb)/total_memory_gb)*100
     
     # Print RAM utilization information
-    print("Total Memory: {:.2f} MB".format(total_memory_mb))
-    print("Available Memory: {:.2f} MB".format(available_memory_mb))
-    print("Used Memory: {:.2f} MB".format(used_memory_mb))
+    print("Total Memory: {:.2f} GB".format(total_memory_gb))
+    print("Available Memory: {:.2f} GB".format(available_memory_gb))
+    print("Used Memory: {:.2f} GB".format(used_memory_gb))
     print("Used Memory Percentage: {:.2f}%".format(used_memory_percentage))
     print("Available Memory Percentage: {:.2f}%".format(available_memory_percent))
 
 if __name__ == "__main__":
     # Call the function to get RAM utilization information
     get_ram_usage()
-
